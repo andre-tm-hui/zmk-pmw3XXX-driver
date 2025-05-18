fd6a0f35bc2849ecfd9d1a08c5032428b064c017
Andre Hui <andre.tm.hui@gmail.com>
2025-05-18 21:33:54 +0100

    a

--- src/pmw33xx.c
+++ src/pmw33xx.c
@@ -762,9 +762,6 @@ static int pmw3360_sample_fetch(const struct device *dev, enum sensor_channel ch
     input_report(dev, config->evt_type, config->y_input_code, y, true, K_NO_WAIT);
   }
 
-  err = gpio_pin_interrupt_configure_dt(&config->irq_gpio,
-    GPIO_INT_LEVEL_ACTIVE);
-
   return err;
 }
 
@@ -797,7 +794,12 @@ static void trigger_handler(struct k_work *work)
   // LOG_INF("data ready handler = %p", handler);
   key = k_spin_lock(&data->lock);
   // if (data->data_ready_handler) {
-    pmw3360_sample_fetch(dev, SENSOR_CHAN_ALL);
+    err = pmw3360_sample_fetch(dev, SENSOR_CHAN_ALL);
+
+  if (!err) {}
+    err = gpio_pin_interrupt_configure_dt(&config->irq_gpio,
+                  GPIO_INT_LEVEL_ACTIVE);
+    }
   // }
   k_spin_unlock(&data->lock, key);
 
