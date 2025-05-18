bc0a9e9aab2b0bc10a56d34b5a0c32d62dad649e
Andre Hui <andre.tm.hui@gmail.com>
2025-05-18 16:51:39 +0100

    t

--- src/pmw33xx.c
+++ src/pmw33xx.c
@@ -954,6 +954,10 @@ static int pmw3360_trigger_set(const struct device *dev,
             const struct sensor_trigger *trig,
             sensor_trigger_handler_t handler)
 {
+  DBG_INF("setting trigger");
+  DBG_INF("trig type %d", trig->type);
+  DBG_INF("trig chan %d", trig->chan);
+  DBG_INF("handler %p", handler);
   struct pmw3360_data *data = dev->data;
   const struct pmw3360_config *config = dev->config;
   int err;
